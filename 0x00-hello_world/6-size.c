21f5ab49f46a6018356d9972c553118e7a93bfd0
Oluwaseun ALX <spreadwebng@gmail.com>
2021-07-09 06:07:54 +0100

    knowing the sizeof various type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ int main (void)
+{
+printf("Size of a char: %d bytes\n" ,sizeof(char));
+printf("Size of a int: %d bytes\n" ,sizeof(int));
+printf("Size of a long int: %d bytes\n" ,sizeof(long int));
+printf("Size of a long long int: %d bytes\n" ,sizeof(long long int));
+printf("Size of a float: %d bytes\n" ,sizeof(float));
+return(0);
+}
